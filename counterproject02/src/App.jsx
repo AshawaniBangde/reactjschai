8218c03ae35b08ab728aaeac79e43e3761cc48a9
geeta <107531267+AshawaniBangde@users.noreply.github.com>
2024-01-09 10:09:34 +0000

    dev config done

--- counterproject02/src/App.jsx
+++ counterproject02/src/App.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function App() {
-    let counter = 15;
-    const addValue = ()=>{
-        // console.log("clicked me",Math.addValue)
-        counter = counter + 1;
-        console.log(counter)
-        
 
-    }
+  const[count,setcount] = useState(15)
+  const[remove,setremove] = useState()
+
+  // let counter = 15;
+  const addValue = () => {
+
+    // console.log("clicked me",Math.addValue)
+    // counter = counter + 1;
+    // console.log(counter)
+
+    setcount(count+1);
+
+
+  }
+  const removeValue = () =>{
+    setremove(remove-1);
+  }
   return (
-    <>    <h1>App</h1>
-     <h2>add and {counter}</h2>
-    <button onClick={addValue}>Add counter</button>
-    <br/>
-    <br/>
-    <button>remove counter</button>
+    <>    
+    
+    <h1>App</h1>
+      <h2>add and{count}</h2>
+      <button onClick={addValue}>Add counter</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>remove counter</button>
     </>
 
   )
